bdf6ba340baf7443616292490962cfd7098b3473
Matthew McCall <mmccall@jhsph.edu>
2014-11-18 04:07:25 +0000

    Added new platform (GPL571, GPL6244, GPL6246) loading to barcode function.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/frma@96840 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/barcode.R
+++ R/barcode.R
@@ -12,22 +12,32 @@ barcode <- function(object, platform=NULL, mu=NULL, tau=NULL, cutoff=6.5, output
 
   if(class(object) %in% c("ExpressionSet", "frmaExpressionSet")){
     if(is.null(platform)){
-      if(annotation(object) %in% c("hgu133a", "hgu133plus2", "mouse4302")){
+      if(annotation(object) %in% c("hgu133a", "hgu133a2", "hgu133plus2", "mouse4302", "hugene.1.0.st.v1", "mogene.1.0.st.v1")){
         if(cleancdfname(annotation(object)) == "hgu133acdf") platform <- "GPL96"
+        if(cleancdfname(annotation(object)) == "hgu133a2cdf") platform <- "GPL571"
         if(cleancdfname(annotation(object)) == "hgu133plus2cdf") platform <- "GPL570"
         if(cleancdfname(annotation(object)) == "mouse4302cdf") platform <- "GPL1261"
+        if(annotation(object) == "hugene.1.0.st.v1") platform <- "GPL6244"
+        if(annotation(object) == "mogene.1.0.st.v1") platform <- "GPL6246"
       } else stop("Microarray platform given by object annotation not recognized -- please supply platform type.")
     } else {
-      if(!platform %in% c("GPL96", "GPL570", "GPL1261")) stop("Platform must be one of: GPL96, GPL570, GPL1261")
+      if(!platform %in% c("GPL96", "GPL570", "GPL571", "GPL1261", "GPL6244", "GPL6246")){
+          stop("Platform must be one of: GPL96, GPL570, GPL571, GPL1261", "GPL6244", "GPL6246")
+      }
     }
     object <- as.matrix(exprs(object))
   }
 
   if(is.null(mu) | is.null(tau)){
-    if(!platform %in% c("GPL96", "GPL570", "GPL1261")) stop("mu and tau must be non-NULL unless platform is one of: GPL96, GPL570, GPL1261")
+    if(!platform %in% c("GPL96", "GPL570", "GPL571", "GPL1261", "GPL6244", "GPL6246")){
+        stop("mu and tau must be non-NULL unless platform is one of: GPL96, GPL570, GPL571, GPL1261, GPL6244, GPL6246")
+    }
     if(platform=="GPL96") pkg <- "hgu133afrmavecs"
     if(platform=="GPL570") pkg <- "hgu133plus2frmavecs"
+    if(platform=="GPL571") pkg <- "hgu133a2frmavecs"
     if(platform=="GPL1261") pkg <- "mouse4302frmavecs"
+    if(platform=="GPL6244") pkg <- "hugene.1.0.st.v1frmavecs"
+    if(platform=="GPL6246") pkg <- "mogene.1.0.st.v1frmavecs"
     require(pkg, character.only=TRUE, quietly=TRUE) || stop(paste(pkg, "package must be installed first"))
     data(list=gsub("frma", "barcode", pkg))
     bcparams <- get(gsub("frma", "barcode", pkg))
@@ -45,19 +55,15 @@ barcode <- function(object, platform=NULL, mu=NULL, tau=NULL, cutoff=6.5, output
 
   if(is.null(names(mu)) | is.null(names(tau))){
     warning("Either mu or tau does not contain names; therefore, matching between object and barcode parameters is not guarenteed.")
-    if(nrow(object)!=length(mu)) stop("Number of rows of object does not equal length of barcode parameters (mu and tau).")
   } else{
     if(any(names(mu)!=names(tau))) stop("Names of mu and tau must be identical.")
   }
 
-  if(!is.null(rownames(object)) & !is.null(names(mu)) & !is.null(names(tau))){
-    i.rm <- which(!rownames(object)%in%names(mu))
-    if(length(i.rm)>0) object <- as.matrix(object[-i.rm,])
-    map <- match(rownames(object),names(mu))
-    mu <- mu[map]
-    tau <- tau[map]
+  if(nrow(object)!=length(mu)){
+      if(platform %in% c("GPL6244", "GPL6246")) message("The following error can likely be fixed by setting target=\"core\" in the frma function.")
+      stop("Number of rows / features of object does not equal length of barcode parameters (mu and tau).")
   }
-
+  
   if(output %in% c("p-value", "lod", "binary")){
     pval <- pnorm(object, mean=mu, sd=tau, lower.tail=FALSE)
     if(output == "p-value"){
